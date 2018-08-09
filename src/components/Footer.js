bb6404aad7370e4aaaebedecbf606321cba662ec
Leonardo M <leo.mi.me@gmail.com>
2018-08-09 04:18:46 -0300

    test build

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => (
-    <footer>
-        <p>Created by @Leomendesm</p>
-    </footer>
-)
-
-export default Footer
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => (
-    <header className="header">
-        <p>Daily Challenges</p>
-    </header>
-)
-
-export default Header
